399d5d00cbae096c19dbbcd8c97a83678b01e68d
iecheverria <iecheverria@gmail.com>
2022-10-31 13:45:36 -0700

    removed full paths to imp_sampcon

--- example/run_clustering.sh
+++ example/run_clustering.sh
@@ -7,8 +7,8 @@ export st=1
 cp ../A_models_clust${cl}_${st}.txt ../scoresA.txt
 cp ../B_models_clust${cl}_${st}.txt ../scoresB.txt
 cp ../../density.txt density.txt
-nohup python /home/ignacia/SOFTW/imp-sampcon/pyext/src/exhaust.py \
-       -n test -p ../ -ra A_models_clust${cl}_${st}_aligned.rmf3 -rb B_models_clust${cl}_${st}_aligned.rmf3 -d density.txt \
-       -m cpu_omp -c 8 -g 2.0 --voxel 3.0 --density_threshold 10 --prism > clustering.log &
+nohup imp-sampcon exhaust \
+      -n test -p ../ -ra A_models_clust${cl}_${st}_aligned.rmf3 -rb B_models_clust${cl}_${st}_aligned.rmf3 -d density.txt \
+      -m cpu_omp -c 8 -g 2.0 --voxel 3.0 --density_threshold 10 --prism > clustering.log &
 
 
