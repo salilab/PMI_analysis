705b675e3303806ad527fd7dff4a53b1592e464d
iecheverria <iecheverria@gmail.com>
2022-10-31 12:29:48 -0700

    Updated run_clustering.sh example to newest version of imp-sampcon with prism

--- example/run_clustering.sh
+++ example/run_clustering.sh
@@ -1,16 +1,14 @@
-module load cuda/9.0.176
-module load imp-fast/last_ok_build
+module load imp
+module load python3/pyrmsd
 
-export top_dir=/home/ignacia/Research/pE-MAP/mod_xls_v2/
-export analys_dir=$top_dir/analys
-export mod_dir=$analys_dir/GSMs_cl0/
-export name=RP_xls
+export cl=1
+export st=1
 
-cp $top_dir/density.txt $mod_dir
-cp $mod_dir/sample_A.txt $mod_dir/Scores_A.txt
-cp $mod_dir/sample_B.txt $mod_dir/Scores_B.txt
+cp ../A_models_clust${cl}_${st}.txt ../scoresA.txt
+cp ../B_models_clust${cl}_${st}.txt ../scoresB.txt
+cp ../../density.txt density.txt
+nohup python /home/ignacia/SOFTW/imp-sampcon/pyext/src/exhaust.py \
+       -n test -p ../ -ra A_models_clust${cl}_${st}_aligned.rmf3 -rb B_models_clust${cl}_${st}_aligned.rmf3 -d density.txt \
+       -m cpu_omp -c 8 -g 2.0 --voxel 3.0 --density_threshold 10 --prism > clustering.log &
 
-ls -lta $analys_dir/GSMs_cl0/sample_A | awk '{print $9}' | grep 'rmf3' > $analys_dir/selected_models_A_cluster0_random.dat
-ls -lta $analys_dir/GSMs_cl0/sample_B | awk '{print $9}' | grep 'rmf3' > $analys_dir/selected_models_B_cluster0_random.dat
 
-nohup python ~/SOFTW/imp-sampcon/pyext/src/Master_Sampling_Exhaustiveness_Analysis.py --sysname $name --path $mod_dir --mode cuda --cores 4 --align --density density.txt --gridsize 1.0 --gnuplot  --rmfs_A $analys_dir/selected_models_A_cluster0_random.dat --rmfs_B $analys_dir/selected_models_B_cluster0_random.dat > clustering.log
