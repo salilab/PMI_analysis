060af5c4cac5fa095f49f85068cb9eb0338655f7
iecheverria <iecheverria@gmail.com>
2022-11-01 11:19:09 -0700

    Fixed deprecated sampcon options

--- example/run_clustering.sh
+++ example/run_clustering.sh
@@ -1,14 +1,18 @@
+#!/bin/bash
 module load imp
 module load python3/pyrmsd
 
 export cl=1
-export st=1
+export st=0
 
 cp ../A_models_clust${cl}_${st}.txt ../scoresA.txt
 cp ../B_models_clust${cl}_${st}.txt ../scoresB.txt
-cp ../../density.txt density.txt
-nohup imp-sampcon exhaust \
-      -n test -p ../ -ra A_models_clust${cl}_${st}_aligned.rmf3 -rb B_models_clust${cl}_${st}_aligned.rmf3 -d density.txt \
-      -m cpu_omp -c 8 -g 2.0 --voxel 3.0 --density_threshold 10 --prism > clustering.log &
+cp ../../density_DDI1_DDI2.txt density.txt
+nohup imp_sampcon exhaust --sysname test --p ../ \
+                          --rmfA A_models_clust${cl}_${st}_aligned.rmf3 \
+                          --rmfB B_models_clust${cl}_${st}_aligned.rmf3 \
+                          --density density.txt --m cpu_omp --cores 8 \
+                          --gridsize 2.0 --voxel 3.0  --density_threshold 10 \
+                          > clustering.log &
 
 
